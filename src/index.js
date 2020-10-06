c4400be067c2257af9f72796f62763135580c25c
harnblaze <harnblaze@gmail.com>
2020-10-06 14:00:57 +0300

    feat: task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    str = str.split("");
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let firstBracket = str[i] === bracketsConfig[j][0];
+            let secondBracket = str[i + 1] === bracketsConfig[j][1];
+            if (firstBracket && secondBracket) {
+                str.splice(i, 2);
+                i = -1;
+            }
+        }
+    }
+    if (str.length === 0) {
+        return true;
+    }
+    return false;
+};
